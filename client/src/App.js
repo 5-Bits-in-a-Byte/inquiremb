3f2e9fb811915feda3d7e6bfce6278deae6e3792
Alec Springel <alecspringel@gmail.com>
2021-02-16 00:16:39 -0800

    Added roboto fonts to client

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import NavigationWrapper from "./components/navigation/NavigationWrapper";
 import Courses from "./components/courses/Courses";
 import SignUp from "./components/signUp/SignUp";
+import "./fonts/fonts.css";
 
 function App() {
   return (
