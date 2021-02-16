879e5474ec04751fef5cce1b16d567b14a0d5463
Alec Springel <alecspringel@gmail.com>
2021-02-16 00:29:23 -0800

    Added logo to navigation bar

--- client/src/components/navigation/TopNavBar.js
+++ client/src/components/navigation/TopNavBar.js
@@ -1,8 +1,13 @@
 import React from "react";
 import styled from "styled-components";
+import Logo from "../../imgs/swag-logo.png";
 
 const TopNavBar = () => {
-  return <Nav></Nav>;
+  return (
+    <Nav>
+      <LogoImg src={Logo} />
+    </Nav>
+  );
 };
 
 export default TopNavBar;
@@ -15,4 +20,11 @@ const Nav = styled.nav`
   left: 0;
   top: 0;
   box-shadow: 0px 1px 6px rgba(0, 0, 0, 0.25);
+  display: flex;
+  align-items: center;
+`;
+
+const LogoImg = styled.img`
+  height: 33px;
+  margin-left: 10px;
 `;
