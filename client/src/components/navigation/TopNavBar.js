cba8ab3a2c9f0c785563d234a01ac72dbce3ff52
Alec Springel <alecspringel@gmail.com>
2021-02-15 23:53:57 -0800

    Created navigation bars and routing

--- client/src/components/navigation/TopNavBar.js
+++ client/src/components/navigation/TopNavBar.js
@@ -1,7 +1,18 @@
 import React from "react";
+import styled from "styled-components";
 
 const TopNavBar = () => {
-  return <div></div>;
+  return <Nav></Nav>;
 };
 
 export default TopNavBar;
+
+const Nav = styled.nav`
+  width: 100vw;
+  height: 55px;
+  background-color: #fff;
+  position: fixed;
+  left: 0;
+  top: 0;
+  box-shadow: 0px 1px 6px rgba(0, 0, 0, 0.25);
+`;
