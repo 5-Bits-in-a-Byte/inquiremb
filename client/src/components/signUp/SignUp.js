b3cc76da934d74aeacf966d0927900b58fb362d1
Brian Gunnarson <bgunnar5@uoregon.edu>
2021-02-17 10:57:46 -0800

    Minor styling, still needs work

--- client/src/components/signUp/SignUp.js
+++ client/src/components/signUp/SignUp.js
@@ -1,41 +1,75 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import img from "../../imgs/signup-background.png";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import GoogleLogin from "react-google-login";
+import SignInLogo from "../../imgs/signin-logo.png";
+import Logo from "../../imgs/swag-logo.png";
 
 const SignUp = () => {
-  const onSuccess = (res) => {
-    console.log("[Login Success] currentUser:", res.profileObj);
-  };
-  const onFailure = (res) => {
-    console.log("[Login Failed] res:", res);
-  };
-
   return (
     <Page>
-      <div>
-        <a href="http://localhost:5000/login">Sign in</a>
-        <Router>
-          <Link to="/login" exact>
-            <GoogleLogin
-              client_id={process.env.GOOGLE_CLIENT_ID}
-              onSuccess={onSuccess}
-              onFailure={onFailure}
-            />
-          </Link>
-          <Switch>
-            <Route path="/login" exact></Route>
-          </Switch>
-        </Router>
-      </div>
+      <Nav>
+        <SignInLogoImg src={SignInLogo} />
+      </Nav>
+      <CenterBlock>
+        <LogoImg src={Logo} />
+        <GoogleButton href="http://127.0.0.1:5000/login">
+          <GoogleLogin />
+        </GoogleButton>
+      </CenterBlock>
+      <Footer>Copyright 5 Bits in a Byte</Footer>
     </Page>
   );
 };
 
 export default SignUp;
 
-const Page = styled.div`
+const Page = styled.body`
   background: url(${img}) no-repeat center center fixed;
   -webkit-background-size: cover;
+  -moz-background-size: cover;
+  -o-background-size: cover;
+  background-size: cover;
+  display: grid;
+`;
+
+const CenterBlock = styled.div`
+  position: relative;
+  background: #ffffff;
+  width: 300px;
+  height: 250px;
+  border-radius: 5px;
+  display: grid;
+  margin: auto;
+`;
+
+const GoogleButton = styled.a`
+  margin: auto;
+`;
+
+const Nav = styled.nav`
+  width: 100vw;
+  height: 55px;
+  position: fixed;
+  left: 0;
+  top: 0;
+  box-shadow: 0px 1px 6px rgba(0, 0, 0, 0.25);
+  display: flex;
+  align-items: center;
+`;
+
+const SignInLogoImg = styled.img`
+  height: 33px;
+  margin-left: 10px;
+`;
+
+const LogoImg = styled.img`
+  height: 25px;
+  margin: auto;
+`;
+
+const Footer = styled.footer`
+  text-align: center;
+  font-size: 10px;
+  color: white;
 `;
