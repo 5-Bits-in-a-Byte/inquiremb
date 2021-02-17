ad5f4890a8ca474cc371cec6b416f33fd28970e1
Brian Gunnarson <bgunnar5@uoregon.edu>
2021-02-16 23:50:09 -0800

    basic login testing

--- client/src/components/signUp/SignUp.js
+++ client/src/components/signUp/SignUp.js
@@ -1,7 +1,42 @@
 import React from "react";
+import styled from "styled-components";
+import img from "../../imgs/signup-background.png";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import GoogleLogin from "react-google-login";
 
 const SignUp = () => {
-  return <div></div>;
+  const onSuccess = (res) => {
+    console.log("[Login Success] currentUser:", res.profileObj);
+  };
+  const onFailure = (res) => {
+    console.log("[Login Failed] res:", res);
+  };
+
+  return (
+    <Page>
+      <div>
+        <Router>
+          <Link to="/login" exact>
+            <GoogleLogin
+              client_id={process.env.GOOGLE_CLIENT_ID}
+              onSuccess={onSuccess}
+              onFailure={onFailure}
+            />
+          </Link>
+          <Switch>
+            <Route path="/login" exact></Route>
+          </Switch>
+        </Router>
+      </div>
+    </Page>
+  );
 };
 
 export default SignUp;
+
+const Page = styled.div`
+  border: 1px solid #000;
+  background-image: url(${img});
+  width: 2000px;
+  height: 900px;
+`;
