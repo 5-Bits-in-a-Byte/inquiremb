93948aabb6d7f8a9bb2207c91e71eae1ff8543d0
Seth Tal <seth.tal255@gmail.com>
2021-02-21 23:03:33 -0800

    Finished rudimentary sidebar component architecture

--- client/src/components/forums_and_posts/ClassView.js
+++ client/src/components/forums_and_posts/ClassView.js
@@ -2,7 +2,7 @@ import React from "react";
 import styled from "styled-components";
 import SectionTab from "./SectionTab";
 
-const ClassView = (params) => {
+const ClassView = (props) => {
   return (
     <Sidebar>
       <ClassTitle>CIS 422</ClassTitle>
@@ -10,10 +10,14 @@ const ClassView = (params) => {
       <hr style={InlineHRStyle}/>
 
       <ClassSection>
-        <SectionTab />
+        <SectionTab setHighlight={true} tabText={"All Posts"} />
+        <SectionTab setHighlight={false} tabText={"Instructor"} />
       </ClassSection>
+      <ClassSubtitle>My Posts</ClassSubtitle>
       <UserSection>
-
+        <SectionTab setHighlight={false} tabText={"My Posts"} />
+        <SectionTab setHighlight={false} tabText={"My Upvoted"} />
+        <SectionTab setHighlight={false} tabText={"Bookmarked"} />
       </UserSection>
     </Sidebar>
   );
@@ -21,31 +25,54 @@ const ClassView = (params) => {
 
 export default ClassView;
 
+const InlineHRStyle = {
+  width : "80%",
+  border: "1px solid #DDDDDD"
+}
+
 const Sidebar = styled.div`
   // border: 1px solid #000;
   width: 200px;
   height: calc(100vh - 55px);
 
-  box-shadow: 5px 2px 6px -2px rgba(0, 0, 0, 0.5);
+  box-shadow: 5px 2px 6px -2px rgba(0, 0, 0, 0.15);
 `;
 
 const ClassTitle = styled.h1`
   // border: 3px solid blue;
   height: 2em;
 
-  line-height: 2em;
+  line-height: 2.5em;
   font-size: 1.5rem;
   text-align: center;
 `;
 
+const ClassSubtitle = styled.h2`
+  height: 2em;
+  margin-left: 1.5em;
+
+  line-height: 2.5em;
+  font-size: 1.25rem;
+  text-align: left;
+
+  color: #B8B8B8;
+`;
+
 const ClassSection = styled.div`
-  border: 3px solid green;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  align-items: center;
+
+  // border: 3px solid green;
+  // height: 5em;
 `;
 
 const UserSection = styled.div`
-  border: 3px solid yellow;
-`;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  align-items: center;
 
-const InlineHRStyle = {
-  width : "75%"
-}
+  // border: 3px solid yellow;
+`;
--- client/src/components/forums_and_posts/SectionTab.js
+++ client/src/components/forums_and_posts/SectionTab.js
@@ -1,16 +1,60 @@
 import React from "react";
+import PropTypes from "prop-types"
 import styled from "styled-components";
 
-const SectionTab = (params) => {
+const SectionTab = (props) => {
+  const shouldHighlight = props.setHighlight === true ? HighlightTab : {marginRight: "0em"};
+
   return (
-    <Tab>
-      
+    <Tab style={shouldHighlight}>
+      <Icon />
+      <TabText>{props.tabText}</TabText>
     </Tab>
   );
 }
 
+SectionTab.propTypes = {
+  setHightlight: PropTypes.bool,
+  tabText: PropTypes.string
+}
+
 export default SectionTab;
 
-const Tab = styled.div`
+const HighlightTab = {
+  borderLeft: "0.3rem solid #4A86FA",
+  background: "rgba(76, 134, 250, 0.09)"
+}
 
+const Tab = styled.div`
+  display: flex;
+
+  // border: 1px solid grey;
+  width: 80%;
+  height: 1.75em;
+  margin: 0.5rem 0;
+
+  align-items: center;
+
+  :hover {
+    cursor: pointer;
+  }
+`;
+
+const TabText = styled.h3`
+  font-size: 1rem;
+
+  transition: 150ms ease-in-out;
+
+  :hover {
+    cursor: pointer;
+  }
+`;
+
+const Icon = styled.img`
+  float: left;
+
+  width: 18px;
+  height: 18px;
+  margin-right: 1em;
+  margin-left: 0.75em;
 `;
