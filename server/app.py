f29e172bfe568c6148e787c44abcd39957132bdd
Sam Peters <sam@sl27.net>
2021-02-25 04:52:52 -0800

    Added swagger and /api/me docs

--- server/app.py
+++ server/app.py
@@ -1,6 +1,7 @@
 from mongo import *
 from flask import Flask, render_template, request, send_from_directory, jsonify
 from flask_restful import Api
+from flasgger import Swagger
 import config
 import os
 # Import API endpoints here:
@@ -18,12 +19,15 @@ app = Flask(__name__, static_url_path="",
 # Allow requests from our localhost (react app in development)
 cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
 
+
 @app.after_request
 def after_request(response):
     response.headers.add('Access-Control-Allow-Origin', '*')
-    response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+    response.headers.add('Access-Control-Allow-Headers',
+                         'Content-Type,Authorization')
     response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
     return response
+
 
 # Adding secret key to app
 app.secret_key = '!secret'
@@ -45,19 +49,19 @@ oauth.register(
 )
 
 api = Api(app, prefix="/api")
-
+swagger = Swagger(app, config=config.swagger_config)
 # register endpoints from /resources folder here:
 api.add_resource(Demo, '/demo')
 api.add_resource(Me, '/me')
 api.add_resource(Courses, '/courses')
 
 
-@ app.route("/")
+# @ app.route("/")
 def hello():
     return render_template("index.html")
 
 
-@app.route("/<path:path>")
+# @app.route("/<path:path>")
 def static_proxy(path):
     """static folder serve"""
     file_name = path.split("/")[-1]
@@ -65,7 +69,7 @@ def static_proxy(path):
     return send_from_directory(dir_name, file_name)
 
 
-@app.errorhandler(404)
+# @app.errorhandler(404)
 def handle_404(e):
     if request.path.startswith("/api/"):
         return jsonify(message="Resource not found"), 404
--- server/config.py
+++ server/config.py
@@ -8,3 +8,21 @@ GOOGLE_CLIENT_ID = os.getenv('GOOGLE_CLIENT_ID')
 GOOGLE_CLIENT_SECRET = os.getenv('GOOGLE_CLIENT_SECRET')
 HS_256_KEY = os.getenv('HS_256_KEY')
 MONGO_URI = os.getenv('MONGO_URI')
+
+swagger_config = {
+    "headers": [
+    ],
+    "specs": [
+        {
+            "endpoint": 'apispec_1',
+            "route": '/apispec_1.json',
+            "rule_filter": lambda rule: True,  # all in
+            "model_filter": lambda tag: True,  # all in
+        }
+    ],
+    "static_url_path": "/flasgger_static",
+    # "static_folder": "static",  # must be set by user
+    "swagger_ui": True,
+    "specs_route": "/apidocs/",
+    "title": "Inquire API"
+}
