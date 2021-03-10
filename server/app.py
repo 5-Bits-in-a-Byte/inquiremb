569a852f154a0aae5c95750262b0cc1eafa400eb
Sam Peters <sam@sl27.net>
2021-03-09 16:29:33 -0800

    Added support for logging in via Github oauth application to backend

--- server/app.py
+++ server/app.py
@@ -53,6 +53,14 @@ oauth.register(
     }
 )
 
+oauth.register(
+    name='github',
+    access_token_url='https://github.com/login/oauth/access_token',
+    authorize_url='https://github.com/login/oauth/authorize',
+    api_base_url='https://api.github.com/',
+    client_kwargs={'scope': 'read:user user:email'},
+)
+
 api = Api(app, prefix="/api")
 swagger = Swagger(app, config=config.swagger_config)
 # register endpoints from /resources folder here:
