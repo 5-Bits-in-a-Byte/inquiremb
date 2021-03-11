12e5a6716baec1f930c990e5a9401116f0d7e4e8
Seth Tal <Seth.tal255@gmail.com>
2021-03-11 15:39:51 -0800

    Modified join course to update Course Card react state (not done)

--- client/src/components/courses/JoinCourse.js
+++ client/src/components/courses/JoinCourse.js
@@ -3,8 +3,42 @@ import Button from "../common/Button";
 import Modal from "../common/Modal";
 import JoinConfirmation from "./joinCourse/JoinConfirmation";
 import JoinInfo from "./joinCourse/JoinInfo";
+import CourseCard from "./CourseCard";
 
-const JoinCourse = (nestedState, nestedSetter) => {
+const AddNewCourseToList = (newCourse, courseList) => {
+  console.log("Course to add: ", newCourse);
+  console.log("Example from list: ", courseList[0]);
+  console.log("Before: ", "\nCourseList: ", courseList);
+
+  let ret = [];
+  for (let i = 0; i < courseList.length; i++) {
+    // console.log(courseList[i]);
+    ret.push(
+      <CourseCard
+        key={courseList[i].props.id}
+        id={courseList[i].props.id}
+        courseName={courseList[i].props.courseName}
+        courseTerm="Winter 2021"
+        color={courseList[i].props.color || "#121212"}
+      />
+    );
+  }
+
+  ret.push(
+    <CourseCard
+      key={newCourse.course_id}
+      id={newCourse.course_id}
+      courseName={newCourse.course_name}
+      courseTerm="Winter 2021"
+      color={newCourse.color || "#121212"}
+    />
+  );
+
+  console.log("After: ", "\nCourseList: ", ret);
+  return ret;
+};
+
+const JoinCourse = ({ courseList, setCourseList }) => {
   const [modalIsShown, toggleModal] = useState(false);
   const [course, joinCourse] = useState(null);
   const [display, toggleDisplay] = useState("flex");
@@ -17,6 +51,9 @@ const JoinCourse = (nestedState, nestedSetter) => {
       {modalIsShown && (
         <Modal
           close={() => {
+            console.log(course);
+            let newCourseList = AddNewCourseToList(course, courseList);
+            setCourseList(newCourseList);
             toggleModal(false);
             toggleDisplay("flex");
           }}
--- client/src/components/home/README-home.md
+++ client/src/components/home/README-home.md
@@ -1,3 +1,7 @@
 # Home
 
-This folder pertains to
+This folder pertains to the files associated with displaying the n most recent posts onto the home page grouped by course.
+
+## Home.js
+
+Home.js is where all of the content for the home page get stored. Home.js sents a get request to the backend server and grabs the 20 most recent posts regardless of ...
