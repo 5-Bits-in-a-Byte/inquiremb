e3bc4e72e6b59c01b1c0c9b2e59b2d286e787478
Sam Peters <sam@sl27.net>
2021-04-08 13:42:26 -0700

    Added mock data to test db on test start

---
+++ server/tests/test_comments.py
@@ -0,0 +1,8 @@
+""" Contains tests for the Me resource """
+import pytest
+from inquire.mongo import *
+
+
+def st_get_comments(client):
+    # Creating posts to add comments
+    assert True == True
