92b439e670da09d33381c85a90a2abc9b6b1c2b1
AARONJVC <avanclea@cs.uoregon.edu>
2021-04-10 14:34:51 -0700

    generic icon prototype based on generic Button with proptypes, not tested

--- client/src/components/common/Icon.js
+++ client/src/components/common/Icon.js
@@ -1,74 +1,43 @@
 import React from "react";
+import PropTypes from "prop-types";
 import styled, { css } from "styled-components";
 
-const Icon = ({ children, loading, onClick, ...props }) => {
-  const clickHandler = loading ? undefined : onClick;
+const Icon = ({ clickable, iconImg, onClick, altMsg, width, ...props }) => {
+  const clickHandler = clickable ? onClick : undefined;
   return (
-    <Btn {...props} onClick={clickHandler}>
-      {loading ? <LoadingDots /> : children}
-    </Btn>
+    <IconDiv {...props} onClick={clickHandler}>
+      <img src={iconImg} alt={altMsg} width={width} />
+    </IconDiv>
   );
+};
+
+Icon.propTypes = {
+  /* Styles the icon to fade in and out on hover */
+  fader: PropTypes.bool,
+  /* Makes the icon width === 100% */
+  autoWidth: PropTypes.bool,
 };
 
 export default Icon;
 
-const Btn = styled.button`
-  cursor: pointer;
+const IconDiv = styled.div`
+  cursor: ${(props) => (props.onClick === undefined ? "default" : "pointer")};
   border: none;
-  padding: 9px 12px;
-  border-radius: 3px;
+  // padding: 9px 12px;
+  // border-radius: 3px;
 
-  font-size: 16px;
+  // font-size: 16px;
   width: ${(props) => props.autoWidth && "100%"};
   display: flex;
   justify-content: center;
   align-items: center;
 
-  // If secondary prop === true
+  // If fader prop === true
   ${(props) =>
-    props.secondary &&
+    props.fader &&
     css`
-      border-radius: 4px;
-      padding: 5px 12px;
-      background-color: #e7e7e7;
-      color: #162b55;
       &:hover {
-        background-color: #dedede;
+        opacity: 50%;
       }
     `}
-
-  // If largeSecondary prop === true
-  ${(props) =>
-    props.largeSecondary &&
-    css`
-      background-color: #e7e7e7;
-      color: #162b55;
-      &:hover {
-        background-color: #dedede;
-      }
-    `}
-
-  // If primary prop === true
-    ${(props) =>
-    props.primary &&
-    css`
-      background-color: #4a86fa;
-      color: #fff;
-      &:hover {
-        background-color: #407df3;
-      }
-    `}
-
-  // If signin prop === true
-    ${(props) =>
-    props.signin &&
-    css`
-        background-color: transparent;
-        color: #fff;
-        width: 10%;
-        &:hover {
-          background-color: white;
-          opacity: 0.85;
-          color: black;
-      `}
 `;
