027c49ac849c56d85d6995306cc6488f4d1bca47
AARONJVC <avanclea@cs.uoregon.edu>
2021-04-26 00:06:46 -0700

    styling and element spacing for user panel

--- client/src/components/configPage/userConfigComponents/UserPanel.js
+++ client/src/components/configPage/userConfigComponents/UserPanel.js
@@ -3,16 +3,12 @@ import PropTypes from "prop-types";
 import styled, { css } from "styled-components";
 import Button from "../../common/Button";
 import Dropdown from "../../common/dropdown/Dropdown";
-import Arrow from "../../../imgs/carrot-down-secondary.svg";
 import TempIcon from "../../../imgs/temporary-user-icon.png";
 
 const UserPerms = { canBan: true, canRemove: true };
 
 const UserPanel = ({ userName, userRole, ...props }) => {
   // --------------------------------------------
-
-  //const [nameField, setNameField] = useState(userName);
-  //const [nameFieldState, setNameFieldState] = useState(true);
 
   return (
     <UserPanelWrapper>
@@ -65,8 +61,8 @@ const UserPanelWrapper = styled.div`
   display: flex;
   align-items: center;
   flex-direction: row;
-  margin: 1rem 0;
-  padding: 1rem 0.5rem 1rem 1rem;
+  margin: 0.75rem 0;
+  padding: 0.5rem 0.25rem 0.5rem 1rem;
   border: 2px solid #e7e7e7;
   border-radius: 5px;
   // Allow a little space between the scroll bar and the panel border
@@ -100,7 +96,10 @@ const UserRoleWrapper = styled.div`
 const AdminActionsWrapper = styled.div`
   display: flex;
   align-items: left;
-  margin: 1rem;
+  justify-content: space-evenly;
+  margin: 1rem 0.25rem;
+  margin-left: auto;
+  width: 275px;
 `;
 
 const UserName = styled.p`
