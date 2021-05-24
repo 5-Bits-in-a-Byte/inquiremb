38f53281c329f168136b7307093d4480a406b41f
Seth Tal <Seth.tal255@gmail.com>
2021-05-23 17:52:52 -0700

    Minor changes

--- client/src/components/context/UserRoleProvider.js
+++ client/src/components/context/UserRoleProvider.js
@@ -1,10 +1,20 @@
-import React, { createContext, useState } from "react";
+import React, { createContext, useEffect, useState } from "react";
+import LazyFetch from "../common/requests/LazyFetch";
 
 const UserRoleContext = createContext(undefined);
 const UserRoleDispatchContext = createContext(undefined);
 
 function UserRoleProvider({ children }) {
   const [userRoleDetails, setUserRoleDetails] = useState(null);
+
+  useEffect(() => {
+    // LazyFetch({
+    //   type: "get",
+    //   endpoint: "",
+    //   onSuccess: () => {},
+    //   onFailure: () => {},
+    // });
+  }, [userRoleDetails]);
 
   return (
     <UserRoleContext.Provider value={userRoleDetails}>
