2fc8c64175665ba05198f6fac116b7d9a6e37484
Seth Tal <Seth.tal255@gmail.com>
2021-05-24 10:30:01 -0700

    Boilerplate for setting up the user profile page

---
+++ client/src/components/userProfile/UserProfile.js
@@ -0,0 +1,14 @@
+import React from "react";
+import styled, { css } from "styled-components";
+
+const UserProfile = ({ props }) => {
+  return (
+    <>
+      <div>
+        <h1>HELL WORLD</h1>
+      </div>
+    </>
+  );
+};
+
+export default UserProfile;
