420de663dbc61a7578ddd21b4172a4a404bee577
Seth Tal <Seth.tal255@gmail.com>
2021-05-27 16:16:28 -0700

    client endpoint changes

--- client/src/components/navigation/ProfileDropdown.js
+++ client/src/components/navigation/ProfileDropdown.js
@@ -13,7 +13,7 @@ const ProfileDropdown = () => {
 
   /** @brief Wrapper for handling signout routing. */
   const handleSignOut = () => {
-    const link = process.env.REACT_APP_SERVER_URL + "/logout";
+    const link = process.env.REACT_APP_SERVER_URL + "/api/auth/logout";
     window.location.href = link;
   };
 
