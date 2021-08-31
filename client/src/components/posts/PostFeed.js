cd058bb91b9b80c31756ae6b4deb29ffe282db3d
Brian Gunnarson <bgunnar5@uoregon.edu>
2021-08-31 15:35:32 -0700

    Fix bug where you couldn't see private post you created

--- client/src/components/posts/PostFeed.js
+++ client/src/components/posts/PostFeed.js
@@ -50,20 +50,23 @@ const createPost = (post, userRole, isCondensed, key) => {
 };
 
 // Sorts the posts by pinned/date
-const generateSections = (data, userRole, isCondensed) => {
+const generateSections = (data, userRole, isCondensed, user) => {
+  console.log("user:", user);
   if (!userRole) return;
 
   let posts = { pinned: [], other: [] };
   let key = 0;
   if (data) {
     data.forEach((post) => {
+      let isMyPost =
+        post.postedBy._id == user._id || post.postedBy._id == user.anonymousId;
       if (post.isPinned) {
-        if (post.isPrivate && userRole.privacy.private)
+        if (post.isPrivate && (userRole.privacy.private || isMyPost))
           posts.pinned.push(createPost(post, userRole, isCondensed, key));
         else if (!post.isPrivate)
           posts.pinned.push(createPost(post, userRole, isCondensed, key));
       } else if (
-        (post.isPrivate && userRole.privacy.private) ||
+        (post.isPrivate && (userRole.privacy.private || isMyPost)) ||
         !post.isPrivate
       ) {
         posts.other.push(createPost(post, userRole, isCondensed, key));
@@ -80,7 +83,7 @@ const fetchData = (endpoint, socketPosts, setData) => {
     type: "get",
     endpoint: endpoint,
     onSuccess: (response) => {
-      // console.log("get request data:", response);
+      console.log("get request data:", response);
       setData([...socketPosts, ...response]);
     },
   });
@@ -184,12 +187,18 @@ const PostFeed = ({ userRole, highlightedSection }) => {
   }, [endpoint]);
 
   const [posts, setPosts] = useState(null);
+  // console.log("posts:", posts);
   const [initialPosts, setInitialPosts] = useState(posts);
 
   // Populate posts and store state of initial posts
   useEffect(() => {
     if (data) {
-      let initialGeneratedPosts = generateSections(data, userRole, isCondensed);
+      let initialGeneratedPosts = generateSections(
+        data,
+        userRole,
+        isCondensed,
+        user
+      );
       setPosts(initialGeneratedPosts);
       setInitialPosts(initialGeneratedPosts);
     }
@@ -209,7 +218,7 @@ const PostFeed = ({ userRole, highlightedSection }) => {
         endpoint: "/courses/" + courseId + "/search?search=" + e.target.value,
         onSuccess: (searchData) => {
           // console.log("onSuccess data:", searchData);
-          setPosts(generateSections(searchData, userRole, isCondensed));
+          setPosts(generateSections(searchData, userRole, isCondensed, user));
         },
         onFailure: (err) => {
           console.log(err.response.data.errors);
