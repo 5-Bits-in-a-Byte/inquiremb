28d9abaf37e156a786581398299f08ae706c84d8
Seth Tal <Seth.tal255@gmail.com>
2021-09-27 22:35:31 -0700

    Started work on improving the leftNavBar

--- client/src/components/navigation/LeftNavBar.js
+++ client/src/components/navigation/LeftNavBar.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 import { useLocation } from "react-router-dom";
 import MenuItem from "./MenuItem";
@@ -8,53 +8,61 @@ import HomeImg from "../../imgs/home-white.svg";
 import CourseImg from "../../imgs/courses-white.svg";
 import MessagesImg from "../../imgs/messages-white.svg";
 import InquireTooltip from "../common/InquireTooltip";
+import { useWindowDimensions } from "../common/CustomHooks";
 
 /** LeftNavBar Component
  * @brief Wrapper containing MenuItems routing the user to the main website pages
  * @returns LeftNavBar component
  */
-const LeftNavBar = () => {
+const LeftNavBar = ({ props }) => {
+  const [hidddenState, setHiddenState] = useState(false);
+
+  const { width, height } = useWindowDimensions();
+
+  useEffect(() => {
+    if (width <= 768) {
+      setHiddenState(true);
+    }
+  }, [width]);
+
   const location = useLocation();
   const active = location.pathname;
+
   return (
-    <Nav>
-      <Wrapper>
-        {/* <InquireTooltip
-          tooltipText={`See recent post history.`}
-          // hoverDelay={1000}
-          customPosition={{
-            top: `25%`,
-            right: `auto`,
-            bottom: `auto`,
-            left: `100%`,
+    <>
+      {hidddenState ? (
+        <BurgerMenu
+          id={"Burger-Menu"}
+          onClick={(event) => {
+            event.stopPropagation();
+            setHiddenState(!hidddenState);
           }}
-        > */}
-        <MenuItem
-          to="/home"
-          label="Recents"
-          img={ClockImg}
-          active={active === "/home"}
-        />
-        {/* </InquireTooltip> */}
-        {/* <InquireTooltip
-          tooltipText={"See your active classes."}
-          // hoverDelay={1000}
-          customPosition={{
-            top: `25%`,
-            right: `auto`,
-            bottom: `auto`,
-            left: `100%`,
-          }}
-        > */}
-        <MenuItem
-          to="/"
-          label="Courses"
-          img={CourseImg}
-          active={active === "/"}
-        />
-        {/* </InquireTooltip> */}
-      </Wrapper>
-    </Nav>
+        >
+          <img
+            src={"https://img.icons8.com/ios-glyphs/30/000000/menu--v1.png"}
+            alt=""
+          />
+        </BurgerMenu>
+      ) : (
+        <></>
+      )}
+      <Nav hide={hidddenState}>
+        <Wrapper>
+          <MenuItem
+            to="/home"
+            label="Recents"
+            img={ClockImg}
+            active={active === "/home"}
+          />
+          <MenuItem
+            to="/"
+            label="Courses"
+            img={CourseImg}
+            active={active === "/"}
+          />
+        </Wrapper>
+      </Nav>
+    </>
   );
 };
 
@@ -69,6 +77,21 @@ const Nav = styled.nav`
   top: 0;
 
   z-index: 9998;
+
+  transition: 150ms ease-out;
+
+  @media only screen and (max-width: 1200px) {
+  }
+  @media only screen and (max-width: 1025px) {
+  }
+  @media only screen and (max-width: 769px) {
+    width: ${(props) => (props.hide ? 0 : "80px")};
+    visibility: ${(props) => (props.hide ? "hidden" : "visible")};
+  }
+  @media only screen and (max-width: 481px) {
+  }
+  @media only screen and (max-width: 400px) {
+  }
 `;
 
 const Wrapper = styled.ul`
@@ -80,5 +103,45 @@ const Wrapper = styled.ul`
   align-items: center;
   justify-content: center;
 
+  transition: 150ms ease-out;
+
+  @media only screen and (max-width: 1200px) {
+  }
+  @media only screen and (max-width: 1025px) {
+  }
+  @media only screen and (max-width: 769px) {
+  }
+  @media only screen and (max-width: 481px) {
+  }
+  @media only screen and (max-width: 400px) {
+  }
+`;
+
+const BurgerMenu = styled.div`
+  position: absolute;
+  top: 66px;
+  left: 0;
+
+  width: 42px;
+  height: 42px;
+  margin: 0.5em;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  background-color: #f8f8f8;
+  border-radius: 5px;
   /* border: 1px solid red; */
+
+  transition: 150ms ease-out;
+
+  :hover {
+    box-shadow: 2px 2px 10px rgba(0, 0, 0, 0.363);
+  }
+
+  :active {
+    box-shadow: 2px 2px 10px rgba(0, 0, 0, 0.363);
+    border: 2px solid #d9d9d9;
+  }
 `;
